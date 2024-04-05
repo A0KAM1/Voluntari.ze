46c756c64aae9fab5ca989ea34f3c6d31cea2f8c
ISADORA MOREIRA DE SOUZA SILVA <0220482112002@fatectq.local>
2024-04-05 19:36:12 -0300

    added web project

---
+++ Voluntari.ze_Web/src/app/sign-up/register-ngo/register-ngo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterNgoComponent } from './register-ngo.component';
+
+describe('RegisterNgoComponent', () => {
+  let component: RegisterNgoComponent;
+  let fixture: ComponentFixture<RegisterNgoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegisterNgoComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(RegisterNgoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
