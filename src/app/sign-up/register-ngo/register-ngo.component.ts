32633641315e8c3f5c138ed2e2b9d802aaf447d8
moreirasisa <moreirassisadora@gmail.com>
2024-04-04 23:10:04 -0300

    create components for pages

---
+++ src/app/sign-up/register-ngo/register-ngo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-ngo',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-ngo.component.html',
+  styleUrl: './register-ngo.component.scss'
+})
+export class RegisterNgoComponent {
+
+}
---
+++ src/app/user/my-subscriptions/my-subscriptions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-subscriptions',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-subscriptions.component.html',
+  styleUrl: './my-subscriptions.component.scss'
+})
+export class MySubscriptionsComponent {
+
+}
